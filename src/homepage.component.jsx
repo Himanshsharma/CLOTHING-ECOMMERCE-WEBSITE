6f6e0cac61cd4cfcceea49f78b8ec4bbe04e9a93
himansh <hssharma2212000@gmail.com>
2020-01-08 15:00:08 +0530

    first coimmit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+const Homepage=()=>{
+    <div className="homepage">
+<div className="directory-menu">
+    <div className="menu-item">
+        <div className="content"></div>
+    </div>
+</div>
+</div>
+
+}
