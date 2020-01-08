bc2ab0efb6a4d24cd5d0c423820789938448162b
himansh <hssharma2212000@gmail.com>
2020-01-09 01:00:18 +0530

    menu itenm

---
+++ src/components/menu-item/Menuitem.component.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+const Menuitem=({title})=>{
+    return (
+        <div className="menu-item">
+        <div className="content">
+            <h1 className="title"> {title}
+                <span className="subtitle">
+shop now
+                </span>
+            </h1>
+        </div>
+    </div>
+</div>
+    )
+}
