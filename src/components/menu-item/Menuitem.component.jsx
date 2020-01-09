974f921d888a351dbf9fd0ae058741b7935ca19f
himansh <hssharma2212000@gmail.com>
2020-01-10 03:41:37 +0530

    styling

--- src/components/menu-item/Menuitem.component.jsx
+++ src/components/menu-item/Menuitem.component.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './menuitem.styles.scss';
 const Menuitem=({title,imgUrl,size})=>{
     return (
-        <div style={{ backgroundImage:`url(${imgUrl})` }} className={ `${size} menu-item`}>
+        <div  className={ `${size} menu-item`}>
+        <div style={{ backgroundImage:`url(${imgUrl})` }} className="back"/>
         <div className="content">
-            <h1 className="title"> {title}
+            <h1 className="title"> {title.toUpperCase()}
+            <br></br>
                 <span className="subtitle">
-shop now
+SHOP NOW
                 </span>
             </h1>
         </div>
