46cff81d615e97bbbe144682a43d824d710c3487
himansh <hssharma2212000@gmail.com>
2020-01-11 18:47:19 +0530

    crown

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Link } from "react-router-dom";
+import {ReactComponent as logo } from '../../assets/crown.svg';
+
+const Header=()=>{
+    return(
+        <div className="header">
+            <Link to="/"></Link>
+        </div>
+    )}
