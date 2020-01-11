947c1a761bd02850a443fc7d551126d8170b31ce
himansh <hssharma2212000@gmail.com>
2020-01-11 16:48:05 +0530

    collection

--- src/components/collection-utem/collections-item.component.jsx
+++ src/components/collection-utem/collections-item.component.jsx
@@ -1,1 +1,15 @@
 import React, { Component } from 'react';
+const CollectionItem=({id , name , imageUrl,price})=>{
+<div className="collection-item">
+<div className="image" style={{backgroundImage=`url(${imageUrl})` }} />
+<div className="collection-footer">
+<span className='name'>{name}</span>
+<span className='price'>{price}</span>
+
+</div>
+
+
+</div>
+
+
+}
