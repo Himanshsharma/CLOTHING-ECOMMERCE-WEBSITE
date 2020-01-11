d5780f1e32d7543d0f0bc7b14a685dd5519850f2
himansh <hssharma2212000@gmail.com>
2020-01-12 01:37:05 +0530

    signin

--- src/components/signin/signin.components.jsx
+++ src/components/signin/signin.components.jsx
@@ -8,18 +8,26 @@ class Signin extends Component {
             password:''
         }
     }
+    handleSubmit=(event)=>{
+event.preventDefault()
+this.setState({email:'',pasword:''})
+    }
+    handleChange=(event)=>{
+        const {value,name}=event.target;
+        this.setState({[name]:value})
+    }
+
     render() { 
         return ( <div className='signin'>
 <h2>I already have an account</h2>
 <span>sign in with your emai</span>
-<form><input type="email" name="email" value={this.state.email } required />
+<form onSubmit={this.handleSubmit}><input type="email" name="email" value={this.state.email } onChange={this.handleChange} required />
 <label>EMAIL</label><br></br>
 <input type="password" name="password" value={this.state.password } required />
 <label>PASSWORD</label><br></br>
 <input type="submit" value="form submit"></input>
 </form>
 </div>  );
-}
-}
+}}
  
 export default Signin;
