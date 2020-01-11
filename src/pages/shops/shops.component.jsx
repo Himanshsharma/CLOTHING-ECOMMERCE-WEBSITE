30671cbb6117e64eebaff364e4f4dada43e8dcaa
himansh <hssharma2212000@gmail.com>
2020-01-11 06:48:26 +0530

    data

--- src/pages/shops/shops.component.jsx
+++ src/pages/shops/shops.component.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SHOP_DATA from './shop.data';
 class Shoppage extends Component {
 
 constructor(){
     super();
-    this.state
+    this.state={
+        collections:SHOP_DATA
+    }
 }
     render() { 
         return (  );
