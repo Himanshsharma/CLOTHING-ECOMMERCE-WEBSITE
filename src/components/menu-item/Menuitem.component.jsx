41bceda18376fe63a1eee4cf38edaaaf0f813053
himansh <hssharma2212000@gmail.com>
2020-01-11 06:40:48 +0530

    commiy

--- src/components/menu-item/Menuitem.component.jsx
+++ src/components/menu-item/Menuitem.component.jsx
@@ -3,7 +3,7 @@ import{ withRouter } from 'react-router-dom';
 import './menuitem.styles.scss';
 const Menuitem=({title,imgUrl,size,history,linkUrl,match})=>{
     return (
-        <div  className={ `${size} menu-item`} onClick={()=>history.push(`${linkUrl}`)} z>
+        <div  className={ `${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)} z>
         <div style={{ backgroundImage:`url(${imgUrl})` }} className="back"/>
         <div className="content">
             <h1 className="title"> {title.toUpperCase()}
