5ab2271f1f14494840e8c88d76cd76d61168a545
himansh <hssharma2212000@gmail.com>
2020-01-26 01:26:19 +0530

    signup

--- src/components/signup/signup.component.jsx
+++ src/components/signup/signup.component.jsx
@@ -9,7 +9,30 @@ class SignUp extends Component {
 email:'',
 password:'',
 confirmpassword:'', }
+handlesubmit=async event =>{
+    event.preventDefault();
+    const {displayName,email,password,confirmpassword}=this.state;
+    if(password != confirmpassword){
+        alert('password donot match confirm password');
+        return;
+    }
+    try{
+const {user} = await auth.createUserWithEmailAndPassword(email,password);
+await createUser(user,{displayName});
+this.setState({
+    displayName:'',
+email:'',
+password:'',
+confirmpassword:'',
+})
+    }
+        catch(error){
+console.log(error)
+        }
+    
+        }
     render() { 
+        const {displayName,email,password,confirmpassword}=this.state;
         return ( 
             <div className="sign-up">
                 <h2 className="title">I Donot have any account</h2>
@@ -17,9 +40,45 @@ confirmpassword:'', }
                 <form className="sign-up-form" onSubmit={this.handlesubmit}>
                     <Forminput
                     type='text'
-                    name="displayName">
+                    name="displayName"
+                    value={displayName}
+                    onChange={this.handlechange}
+                    label='display name'
+                    required
+                    >
 
                     </Forminput>
+                    <Forminput
+                    type='email'
+                    name="email"
+                    value={email}
+                    onChange={this.handlechange}
+                    label='Email'
+                    required
+                    >
+
+                    </Forminput>
+                    <Forminput
+                    type='password'
+                    name="password"
+                    value={password}
+                    onChange={this.handlechange}
+                    label='Password'
+                    required
+                    >
+
+                    </Forminput>
+                    <Forminput
+                    type='password'
+                    name="confirmPassword"
+                    value={confirmpassword}
+                    onChange={this.handlechange}
+                    label='Confirm Password'
+                    required
+                    >
+
+                    </Forminput>
+                    <Custombutton type="submit">SIGN UP</Custombutton>
 
                 </form>
 
